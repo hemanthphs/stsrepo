99dffb71384adb1264444ee33d624a006c706f23
hemanthphs <hemanthphs10@gmail.com>
2021-09-20 10:12:26 +0530

    new loginbranch is commited

---
+++ testsep/src/testsep/LoginBranch.java
@@ -0,0 +1,13 @@
+package testsep;
+
+public class LoginBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private String username;
+		private String password;
+
+	}
+
+}
+  
